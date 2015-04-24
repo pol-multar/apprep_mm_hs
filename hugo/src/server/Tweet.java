c13db67b4d9ee6b43d2fcec6fd7633633d24d939
Hugo Simond <hugosimond@gmail.com>
2015-04-24 17:47:05 +0200

    Le RMI fonctionne !

---
+++ hugo/src/server/Tweet.java
@@ -0,0 +1,57 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Tweets
+ * 
+ * @author Hugo
+ *
+ */
+public class Tweet {
+    protected int id;
+    protected String author;
+    protected String message;
+    protected List<String> hashtags;
+
+    public Tweet(int id, String author, String message) {
+        this.id = id;
+        this.author = author;
+        this.message = message;
+        hashtags = new ArrayList<String>();
+        String[] words = message.split(" ");
+        for(String word : words){
+            if(word.substring(0, 1).equals("#")){
+                hashtags.add(word);
+            }
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return "[n°" + id + "] " + author + " a tweeté : " + message;
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return id;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getAuthorSource() {
+        return author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public List<String> getHashtags() {
+        return hashtags;
+    }
+}
