f3bf518b27331b0897afbdb0364c218fed78617c
Maxime MULTARI <maxime.multari@neuf.fr>
2015-05-22 19:17:18 +0200

    Ajout d'un topic pour test

--- projetTwitter/src/main/java/fr/unice/polytech/si4/apprep/serveur/TwitterServer.java
+++ projetTwitter/src/main/java/fr/unice/polytech/si4/apprep/serveur/TwitterServer.java
@@ -1,15 +1,15 @@
 package fr.unice.polytech.si4.apprep.serveur;
 
-import javax.jms.Connection;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
+import javax.jms.*;
+import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -21,12 +21,11 @@ public class TwitterServer extends UnicastRemoteObject implements TwitterRemote 
     private ConcurrentMap<Integer, Tweet> tweets;
     private List<String> availableHashtags;
     private ConcurrentMap<String,String> logins;
-    private Session sendSession = null;
-    private MessageProducer sender = null;
-    private Queue queue = null;
-    InitialContext context = null;
     //JMS Part
     private Connection connect;
+    private Session sendSession = null;
+    private MessageProducer sender = null;
+    InitialContext context = null;
 
 
     protected TwitterServer() throws RemoteException {
@@ -35,6 +34,54 @@ public class TwitterServer extends UnicastRemoteObject implements TwitterRemote 
         logins = new ConcurrentHashMap<String,String>();
         availableHashtags = new ArrayList<String>();
         System.out.println("Serveur lancé !");
+    }
+
+    private void configuration() throws JMSException {
+        try
+        {	// Create a connection
+            // Si le producteur et le consommateur étaient codés séparément, ils auraient eu ce même bout de code
+
+            Hashtable properties = new Hashtable();
+            properties.put(Context.INITIAL_CONTEXT_FACTORY,
+                    "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+            properties.put(Context.PROVIDER_URL, "tcp://localhost:61616");
+
+            context = new InitialContext(properties);
+
+            ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+            connect = factory.createConnection();
+
+            this.configurerPublisher();
+            //connect.start(); // on peut activer la connection.
+        } catch (javax.jms.JMSException jmse){
+            jmse.printStackTrace();
+        } catch (NamingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        this.publier();
+    }
+
+    private void configurerPublisher() throws JMSException, NamingException{
+        // Dans ce programme, on decide que le producteur decouvre la queue (ce qui la crééra si le nom n'est pas encore utilisé)
+        // et y accedera au cours d'1 session
+        sendSession = connect.createSession(false, javax.jms.Session.AUTO_ACKNOWLEDGE);
+        Topic topic = (Topic) context.lookup("dynamicTopics/topicExo2");
+        sender = sendSession.createProducer(topic);
+    }
+
+    private void publier() throws JMSException{
+        for (int i=1;i<=10;i++){
+            //Fabriquer un message
+            MapMessage mess = sendSession.createMapMessage();
+            mess.setInt("num",i);
+            mess.setString("nom",i+"-");
+            if (i%2==0)
+                mess.setStringProperty("typeMess","important");
+            if (i==1) mess.setIntProperty("numMess",1);
+            //Poster ce message dans la queue
+            sender.send(mess); // equivaut à publier dans le topic
+        }
     }
 
     @Override
@@ -88,6 +135,12 @@ public class TwitterServer extends UnicastRemoteObject implements TwitterRemote 
         } catch (RemoteException e) {
             e.printStackTrace();
             return;
+        }
+
+        try {
+            ts.configuration();
+        } catch (JMSException e) {
+            e.printStackTrace();
         }
     }
 }
